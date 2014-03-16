36fd777cf333e9dd01123e83fad5c7eedd2febfb
Ahmed Nadar <ahmedfay@hotmail.com>
2014-03-16 17:40:07 -0400

    Add Movie class wich it includes movie objects. Check the last if...

--- movie.rb
+++ movie.rb
@@ -1,4 +1,7 @@
 class	Movie	
+	attr_reader :title
+	attr_accessor :rank
+
 	def initialize(title, rank=0)
 	  @title = title.capitalize
 	  @rank = rank
@@ -15,14 +18,21 @@ class	Movie
 	def thumbs_down
 		@rank -= 1
 	end
-
 end
 
+if __FILE__ == $0
+	movie1 = Movie.new("gravity", 20)
+	puts movie1.thumbs_up
+	puts movie1.title
+	puts movie1
 
-movie1 = Movie.new("gravity", 20)
-movie1.thumbs_up
-puts movie1
+	movie2 = Movie.new("wall street", 3)
+	puts movie2.thumbs_down
+	puts movie2.rank
+	puts movie2
 
-movie2 = Movie.new("Wall Street", 20)
-movie2.thumbs_down
-puts movie2
+	movie3 = Movie.new("frozen", 3)
+	puts movie3.thumbs_up
+	puts movie3.rank
+	puts movie3
+end
