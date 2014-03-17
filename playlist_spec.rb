f1da731bb01f897ab90d8fce794fbdd65f9e5155
Ahmed Nadar <ahmedfay@hotmail.com>
2014-03-17 02:33:53 -0400

    More tezt about consumtion carbs

--- playlist_spec.rb
+++ playlist_spec.rb
@@ -14,32 +14,50 @@ describe ::Playlist do
 
 		it "gives the movie thumbs up if a high number is rolled" do
 			Reviewer.stub(:roll_die).and_return(20)
-			playlist.play
-			expect(movie.rank).to eql(initial_rank+1)
+			playlist.play(2)
+			expect(movie.rank).to eql(initial_rank+2)
 
 			Reviewer.stub(:roll_die).and_return(19)
-			playlist.play
-			expect(movie.rank).to eql(initial_rank+2)
+			playlist.play(2)
+			expect(movie.rank).to eql(initial_rank+4)
 		end
 		
 		it "skips the movie if a medium number is rolled" do
 			Reviewer.stub(:roll_die).and_return(10)
-			playlist.play
+			playlist.play(2)
 			expect(movie.rank).to eql(initial_rank)
 
 			Reviewer.stub(:roll_die).and_return(13)
-			playlist.play
+			playlist.play(2)
 			expect(movie.rank).to eql(initial_rank)
 		end
 
 		it "gives the movie thumbs down if a lower number is rolled" do
-			Reviewer.stub(:roll_die).and_return(5)
-			playlist.play
-			expect(movie.rank).to eql(initial_rank-1)
+			Reviewer.stub(:roll_die).and_return(2)
+			playlist.play(2)
+			expect(movie.rank).to eql(initial_rank-2)
 
 			Reviewer.stub(:roll_die).and_return(4)
-			playlist.play
-			expect(movie.rank).to eql(initial_rank-2)
+			playlist.play(2)
+			expect(movie.rank).to eql(initial_rank-4)
 		end
+  end
+
+  context 'with movies that have casued snacks to be consumed' do
+  	before	do
+  		movie1 = Movie.new("gravity") 
+	  	movie2 = Movie.new("godfather") 
+			
+			playlist.add_movie(movie1)
+			playlist.add_movie(movie2)
+
+	  	movie1.ate_snack(Snack.new(:popcorn, 10))
+	  	movie1.ate_snack(Snack.new(:popcorn, 10))
+	  	movie2.ate_snack(Snack.new(:juice, 5))
+  	end
+
+  	it "computes total carbs consumed as the sum of all mocies carbs consumed" do
+  		expect(playlist.total_carbs_consumed).to eql(25)
+  	end
   end
 end
